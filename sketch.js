174ece74257a7a16b5dafa95b841aff8b7e14499
runtelol <75100333+runtelol@users.noreply.github.com>
2021-03-11 15:14:45 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+const Engine=Matter.Engine;
+const Bodies=Matter.Bodies
+const World=Matter.World
+var engine, world, ground;
+var box, log;
+function setup() {
+  createCanvas(400,800);
+  engine=Engine.create()
+  world=engine.world
+  ground=new Ground(200,780,400,20)
+  box=new Box(200,300)
+  log=new Log(123,123,PI)
+}
+
+function draw() {
+  background(0,0,0);
+  Engine.update(engine);
+  ground.display();
+  box.display();
+  log.display();
+}
